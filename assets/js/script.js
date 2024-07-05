4f91f3b20f2e4921fa13a0ddd077cdcdf524e717
Connor_OHalloran <HCnamehere@protonmail.com>
2024-07-04 23:53:44 -0400

    testing data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-var requestUrl = "https://randomuser.me/api/?results=9";
+var apiUrl = "https://archive.org/advancedsearch.php?q=subject:google+sheets&output=json";
 
-fetch(requestUrl)
-.then(function(response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-});
+var listEl = document.getElementById("myData");
+
+fetch(apiUrl)
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(function(data) {
+        var docArray = data.response.docs;
+        for(var i = 0; i < docArray.length; i++) {
+            var listItem = document.createElement("li");
+            listItem.textContent = docArray[i].description;
+            listEl.appendChild(listItem);
+        }
+    })
+    .catch(function(error) {
+        console.log('Fetch error: ', error);
+    });
