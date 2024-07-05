fd562eee352d50fef3ef41b82c3ea460c7c1a47b
Connor_OHalloran <HCnamehere@protonmail.com>
2024-07-04 23:28:40 -0400

    more test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var requestUrl = "https://randomuser.me/api/?results=5";
+var requestUrl = "https://randomuser.me/api/?results=9";
 
 fetch(requestUrl)
-.then(function(response){
+.then(function(response) {
     return response.json();
 })
 .then(function (data) {
